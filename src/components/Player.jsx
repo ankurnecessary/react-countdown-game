9ba596c4b7c989340fbd16888717b3e8dfecffe6
Ankur Gupta <ankurnecessary@gmail.com>
2024-02-27 04:14:30 +1030

    Resetting the value of player name <input /> to "" after submit

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,17 +3,16 @@ import { useState } from "react";
 
 export default function Player() {
   const playerName = useRef();
-  const [enteredPlayerName, setEnteredPlayerName] = useState("");
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
 
   function submitHandler() {
     setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = "";
   }
 
   return (
     <section id="player">
-      <h2>
-        Welcome {enteredPlayerName ? enteredPlayerName : "unknown entity"}
-      </h2>
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
         <input ref={playerName} type="text" />
         <button onClick={submitHandler}>Set Name</button>
